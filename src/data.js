ba472dd8e10af12c0f0fb649381cfdca9e36f82a
josenauto <jemilionautoch@gmail.com>
2020-12-10 19:36:06 -0300

    Created a file with data

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    email: "bertieyates@gmail.com",
+    image:
+      "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    email: "hesterhogan@gmail.com",
+    image:
+      "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    email: "larrylittle@gmail.com",
+    image:
+      "https://images.pexels.com/photos/842548/pexels-photo-842548.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    email: "seanwalsh@gmail.com",
+    image:
+      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    email: "lolagardner@gmail.com",
+    image:
+      "https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+];
