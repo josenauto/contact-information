027aac7cd6ae2805149d3b52b70c6983a23894cf
josenauto <jemilionautoch@gmail.com>
2020-12-10 21:01:22 -0300

    Created a component called list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 import data from "./data";
+import List from "./List";
 
 function App() {
   const [contact, setContact] = useState(data);
   return (
     <div className="App">
       <h3>You have {contact.length} contacts</h3>
+      <List />
     </div>
   );
 }
